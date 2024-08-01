32d9d22fc453c92befab9f0a121f48debdb58eb6
gooptapuja <49068583+gooptapuja@users.noreply.github.com>
2024-08-01 16:54:46 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail = "pooja1@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+let accountState = '0';
+
+// accountId = 2// not allowed
+
+accountEmail ="abcf567@gmail.com"
+accountPassword = "9080"
+accountCity = "gwalior"
+console.log(accountCity);
+
+/* PREFER NOT USE VAR-
+because of issue in block scope and functional scope*/
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
