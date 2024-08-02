8d566462ce9b230ffe6953da2fb7967b49a336ad
gooptapuja <49068583+gooptapuja@users.noreply.github.com>
2024-08-02 03:27:21 +0000

    operations in js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,8 @@
 let score = null;
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 //"33" =>33
 //"33abc" =>NaN
@@ -14,7 +14,25 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 
 let Number1;
 let stringCheck = String(Number1)
-console.log(stringCheck);
-console.log(typeof stringCheck);
+// console.log(stringCheck);
+// console.log(typeof stringCheck);
+
+// *******************OPERTAIONS*****************************//
+// console.log(2+2);
+ let str1 = "pooja"
+ let str2;
+ let str3 = Number(str2);
+//  console.log(str1+str3);
+//  console.log(typeof str3);
+ console.log(2+3+"7");
+ console.log("2"+1+1);
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+ 
+
+ 
+ 
+ 
+
+
 
 
