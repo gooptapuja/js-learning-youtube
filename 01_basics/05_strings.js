fd745e25c11573d7bacd956175a9e40aff85cf5c
gooptapuja <49068583+gooptapuja@users.noreply.github.com>
2024-08-04 17:39:14 +0000

    String in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const a = "puja"
+const b = 80
+// console.log(a+b);
+
+// console.log(a+b+"value");
+
+// console.log(`Here is my name ${a} + and the value is ${b}`);
+// const gameName = new String('hi pooja');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('i'));
+// 
+
+const greeting = "    hii buddy   "
+console.log(greeting);
+console.log(greeting.trim());
+const url = "http://youtube//channel  com"
+console.log(url.replace(" ","--"));
+
+
+ 
+
+
+
+// console.log(newMenber);
+
+
+
+
+
+
+
+
+
+
+
