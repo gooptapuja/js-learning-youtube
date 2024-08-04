3bce17210372f5dc4adec13b324a6fa174620506
gooptapuja <49068583+gooptapuja@users.noreply.github.com>
2024-08-04 18:24:20 +0000

    Number and Math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,29 @@
+const score = 800
+console.log(score);
+const balance = new Number(100)
+// console.log(typeof balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(2));
+const othernum = 1234.9087
+console.log(othernum.toPrecision(6));
+
+// +++++++++++++++++Math++++++++++++++
+
+// console.log(Math); --0 or 1 k bich m value
+// console.log(Math.abs(-4));
+// console.log(Math.sqrt(25));
+console.log(Math.random()*10+1);
+console.log(Math.random());
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+
+
+
+
+
+
+
+
+
