b6174f1de69a5465e9bafe2462034415dd72b43c
gooptapuja <49068583+gooptapuja@users.noreply.github.com>
2024-08-05 17:39:18 +0000

    Arrays in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+const myArr = [10,11,21,31,41,51,61]
+// const myArrName = ["heros","superheros","leaders"]
+
+// const myArr1 = new Array(1,2,3,4,5)
+console.log(myArr);
+
+// array methods
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(2));
+// const newArray = myArr.join()
+// console.log(myArr);
+// console.log(newArray);
+
+//slice,splice 
+
+// console.log("A-", myArr)
+// console.log("Slice-",myArr.slice(1,6));
+// const myn1 = myArr.splice(2, 3)
+// console.log(myn1);
+
+
+
+
+
+
+
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,22 @@
+const student = ["hiteshi","pooja","priya","priti"]
+const family = ["maa","papa","sister"]
+// student.push(family)
+// const b=student.concat(family)
+// console.log(b);
+
+// const c = [...student, ...family]//-spread operator
+// console.log(c);
+
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh"));
+console.log(Array.from({name:"hitesh"}));//*important
+
+const a = 10
+const b = 'A'
+const c = 30
+
+console.log(Array.of(a,b,c));
+
+
+
+
