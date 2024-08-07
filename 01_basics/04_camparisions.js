2cd0fdc02e93d411270e62dc8e5a1aab4ad2999c
gooptapuja <49068583+gooptapuja@users.noreply.github.com>
2024-08-07 17:43:55 +0000

    coersion operation in js

--- 01_basics/04_camparisions.js
+++ 01_basics/04_camparisions.js
@@ -1,9 +1,17 @@
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(2>1);
-console.log("1">2);
+// console.log(2>1);
+// console.log("1">2);
+
+let a = 10
+let b = Number(a)
+
+
+console.log(typeof b);
+
+console.log(a===b);//type coercion 
 
 
 
